66018ace437ec6b08d6f8be3e1e13f6f44f9c7ba
Uday-1402 <udayt1402@gmail.com>
2025-01-25 17:49:25 +0530

    require + Modifiers code added

---
+++ contracts/Modifiers.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract PausableToken {
+    address public owner;
+    bool public paused;
+    mapping(address => uint) public balances;
+    event Balance(address owner,uint bal);
+
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner() {
+        // 1️⃣ Implement the modifier to allow only the owner to call the function
+        require(msg.sender == owner,"You are not the owner!!");
+        _;//_ tells the when modifier is applied where should the function run i.e. the function pause will run after the above lines of code.
+    }
+
+    // 2️⃣ Implement the modifier to check if the contract is not paused
+    modifier notPaused(){
+        require(paused == false,"Contract is paused!!");
+        _;
+    }
+
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    // 3️⃣ use the notPaused modifier in this function 
+    function transfer(address to, uint amount) public notPaused {
+        emit Balance(msg.sender, balances[msg.sender]);
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
